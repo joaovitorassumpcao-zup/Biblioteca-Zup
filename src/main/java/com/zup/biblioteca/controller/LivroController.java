96e6be416bebbafbd817e9181b034adc35c25a49
joaovitorassumpcao-zup <joao.assumpcao@zup.com.br>
2023-07-28 11:48:53 -0300

    Misc

--- src/main/java/com/zup/biblioteca/controller/LivroController.java
+++ src/main/java/com/zup/biblioteca/controller/LivroController.java
@@ -1,7 +1,7 @@
 package com.zup.biblioteca.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class LivroController {
 }
--- src/main/java/com/zup/biblioteca/repository/LivroRepository.java
+++ src/main/java/com/zup/biblioteca/repository/LivroRepository.java
@@ -1,8 +1,10 @@
 package com.zup.biblioteca.repository;
 
+import com.zup.biblioteca.model.LivroModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LivroRepository {
+public interface LivroRepository extends JpaRepository<LivroModel, Long> {
 
 }
