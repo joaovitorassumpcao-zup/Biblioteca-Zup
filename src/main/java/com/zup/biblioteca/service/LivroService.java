b01533b5fcd2b62eb1ae44146b7b6b9ba4f1f63a
joaovitorassumpcao-zup <joao.assumpcao@zup.com.br>
2023-07-28 15:57:18 -0300

    LivroService

--- src/main/java/com/zup/biblioteca/service/LivroService.java
+++ src/main/java/com/zup/biblioteca/service/LivroService.java
@@ -1,7 +1,36 @@
 package com.zup.biblioteca.service;
 
+import com.zup.biblioteca.model.LivroModel;
+import com.zup.biblioteca.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LivroService {
+
+    @Autowired
+    LivroRepository livroRepository;
+
+    public List<LivroModel> getAllLivros() {
+        return livroRepository.findAll();
+    }
+
+    public LivroModel getLivroById(Long id) {
+        return livroRepository.findById(id).orElseThrow();
+    }
+
+    public LivroModel createLivro(LivroModel livro) {
+        return livroRepository.save(livro);
+    }
+
+    public LivroModel updateLivro(LivroModel livro) {
+        return livroRepository.save(livro);
+    }
+
+    public void deleteLivro(Long id) {
+        livroRepository.deleteById(id);
+    }
 }
